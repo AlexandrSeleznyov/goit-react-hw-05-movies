2dfdfc4bd3b2aea566a714d073fb69c95681999e
AlexandrSeleznyov <vuavso16@gmail.om>
2021-09-30 21:09:15 +0300

    Updates

---
+++ static/js/9.5b651f8e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[9],{70:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var i=n(1);function o(){return Object(i.jsx)("h2",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=9.5b651f8e.chunk.js.map
